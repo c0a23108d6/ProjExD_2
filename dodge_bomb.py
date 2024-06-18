589db27b5a8a62c4b6a2bb00ae80d7020127b0e2
nakajimam <c0a23108d6@edu.teu.ac.jp>
2024-06-18 16:14:27 +0900

    演習３の途中(1)

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import random
 import sys
 import pygame as pg
+import time
 
 
 WIDTH, HEIGHT = 1400, 750
@@ -10,7 +11,17 @@ DELTA = {  #移動量辞書
     pg.K_DOWN: (0, 5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (5, 0)
-    }
+}
+"""KAITEN = {  #演習１
+    (-5, -5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 45, 2.0),
+    (-5, 0): pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0),
+    (-5, 5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 315, 2.0),
+    (0, 5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 270, 2.0),
+    (5, 5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 225, 2.0),
+    (5, 0): pg.transform.rotozoom(pg.image.load("fig/3.png"), 180, 2.0),
+    (5, -5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 135, 2.0),
+    (0, -5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 90, 2.0),
+}"""
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -28,10 +39,19 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     return yoko, tate
 
 
+"""def kaiten():  #演習１
+    for k, v in KAITEN.items():
+        if KAITEN[3]:
+            pg.transform.flip(pg.image.load("fig/3.png"), True, False)
+        if sum_mv == 
+        """
+
+            
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
@@ -43,13 +63,23 @@ def main():
     vx, vy = 5, -5
     clock = pg.time.Clock()
     tmr = 0
+    go_img = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(go_img, (0,0,0), (0, 0, WIDTH/2, HEIGHT/2))
+    go_rct = go_img.get_rect()
+    fonto = pg.font.Font(None, 80)
+    txt = fonto.render("Game Over", True, (255, 255, 255))
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        screen.blit(bg_img, [0, 0])
         if kk_rct.colliderect(bb_rct):
+            screen.blit(go_img, go_rct)
+            screen.blit(txt, [WIDTH, HEIGHT])
+            time.sleep(5)
             return
-        screen.blit(bg_img, [0, 0]) 
+        #screen.blit(bg_img, [0, 0]) 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, v in DELTA.items():
@@ -67,6 +97,7 @@ def main():
         if not tate:
             vy *= -1
         screen.blit(kk_img, kk_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
